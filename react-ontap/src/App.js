d4861362eb314c129888a9aa7ce9172fdf705455
Ryan <ryan.d.elliott@gmail.com>
2017-11-21 00:30:40 -0700

    Add axios methods

--- react-ontap/src/App.js
+++ react-ontap/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './lib/css/bootstrap.min.css'; /* Bootstrap 4 */
 import './App.css';
 import { TapListComponent, TapFormComponent } from './components';
@@ -8,62 +9,36 @@ class App extends Component {
     super(props);
     this.state = {
       tapLocations: ['Central', 'North', 'Upstairs', 'South'],
-      taps: [
-      {
-      _id: "5a123f7649d38b037ed4dc3a",
-      name: "Central",
-      lastUpdated: "2017-11-20T02:35:34.359Z",
-      __v: 1,
-      beers: [
-      "Breck Vanilla Porter",
-      "Ska Modus Hoperandi",
-      "Dry Dock Apricot Blonde",
-      "Lager"
-      ]
-      },
-      {
-      _id: "5a123f7649d38b037ed4dc3c",
-      name: "North",
-      lastUpdated: "2017-11-20T02:35:34.359Z",
-      __v: 0,
-      beers: [
-      "DBC Pretzel Assassin",
-      "DBC Graham Cracker Porter",
-      "TRVE Cursed"
-      ]
-      },
-      {
-      _id: "5a123f7649d38b037ed4dc3b",
-      name: "Upstairs",
-      lastUpdated: "2017-11-20T02:35:34.359Z",
-      __v: 2,
-      beers: [
-      "New Holland Dragons Milk",
-      "Cerebral Dark Galaxy",
-      "Epic Tart and Juicy",
-      "SKA IPA",
-      "Old Chub"
-      ]
-      },
-      {
-      _id: "5a123f7649d38b037ed4dc3d",
-      name: "South",
-      lastUpdated: "2017-11-20T02:35:34.359Z",
-      __v: 1,
-      beers: [
-      "Ska True Blonde",
-      "Oskar Blues Dales Pale Ale",
-      "Oskar Blues Old Chub",
-      "Stout"
-      ]
-      }
-      ]
+      taps: []
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3001/taps')
+      .then(res => {
+        /* add data to state */
+        this.setState({
+          taps: res.data
+        });
+      });
   }
 
   handleSubmit(event, tapName, beerName){
     event.preventDefault();
-    console.log({tapName: tapName, beerName: beerName});
+    console.log({tapName: tapName, beer: beerName});
+    /* API POST */
+    axios.post('http://localhost:3001/taps',{tapName: tapName, beer: beerName})
+      .then(res => {
+        /* Update state */
+        let currentTaps = this.state.taps.map(tap => {
+          if(tap.name === res.data.name){
+            return res.data;
+          }
+          return tap;
+        });
+        this.setState({taps: currentTaps});
+      });
   }
 
   render() {
